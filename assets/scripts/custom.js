78b2504a465dc77b46bb6c4eac655426097b620a
y.vlasenko <vlasenkoj@gmail.com>
2016-04-01 18:50:41 +0300

    added images for video

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,9 +1,3 @@
-/*!
- * LoveHub - Multipurpose Non-Profit Theme
- * Copyright 1994-2015 ForBetterWeb.com
- * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
- */
-
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -35,12 +29,18 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Countdown
-$('#clock').countdown('2016/10/31').on('update.countdown', function(event) {
-    var $this = $(this).html(event.strftime(''
-    + '<div><span>%-w</span>week%!w</div>'
-    + '<div><span>%-d</span>day%!d</div>'
-    + '<div><span>%H</span>hr</div>'
-    + '<div><span>%M</span>min</div>'
-    + '<div><span>%S</span>sec</div>'));
+$( "#contact-form" ).validVal();
+$( "#contact-form button:submit" ).click(function( event ) {
+    event.preventDefault();
 });
+
+$(function()
+{
+    $('#contact-form').submit(function(){
+        $("input[type='submit']", this)
+            .val("Зачекайте...")
+            .attr('disabled', 'disabled');
+        return true;
+    });
+});
+
